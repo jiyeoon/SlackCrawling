43858c7b6b1bcd5906b8c531cab787fc175a55a5
jiyeon <i3048i@naver.com>
2020-01-11 16:41:29 +0900

    add chromedriver and crawling code

---
+++ commit_crawling.py
@@ -0,0 +1,31 @@
+# commit crawling.py
+
+import time
+import requests
+from bs4 import BeautifulSoup
+import os
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "slack.settings")
+
+import django
+django.setup()
+
+from .outgoingslack.models import OutGoingSlack
+from collections import namedtuple
+
+from selenium import webdriver
+
+driver = webdriver.Chrome(executable_path="./chromedriver")
+
+def getCommitList():
+    driver.get('https://app.slack.com/client/TS08LHC73/CSAFNFB4M')
+
+    driver.find_element_by_id('domain').send_keys('githubfarm')
+    driver.find_element_by_xpath('//*[@id="submit_team_domain"]').click()
+
+    driver.find_element_by_name('email').send_keys('i3048i@yonsei.ac.kr')
+    driver.find_element_by_xpath('//*[@id="signin_btn"]').click()
+
+    req = driver.page_source
+    soup = BeautifulSoup(req, 'html.parser')
+    message_list = soup.select('div.c-message_kit__gutter')
