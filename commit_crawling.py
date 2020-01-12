7aa9b9fc81425be84d2193e720698e1d917c035f
jiyeon <i3048i@naver.com>
2020-01-12 13:39:54 +0900

    add..

--- commit_crawling.py
+++ commit_crawling.py
@@ -13,6 +13,7 @@ django.setup()
 from .outgoingslack.models import OutGoingSlack
 from collections import namedtuple
 
+
 from selenium import webdriver
 
 driver = webdriver.Chrome(executable_path="./chromedriver")
@@ -24,8 +25,18 @@ def getCommitList():
     driver.find_element_by_xpath('//*[@id="submit_team_domain"]').click()
 
     driver.find_element_by_name('email').send_keys('i3048i@yonsei.ac.kr')
+
     driver.find_element_by_xpath('//*[@id="signin_btn"]').click()
 
     req = driver.page_source
     soup = BeautifulSoup(req, 'html.parser')
     message_list = soup.select('div.c-message_kit__gutter')
+
+    commit_list = []
+    for i in range(len(message_list)):
+        temp = message_list[i].select('a.c-message__sender_link')
+        if temp:
+            if(temp[0].text == "GitHub"):
+                commit_list.append(message_list[i])
+
+    return commit_list
--- outgoingslack/views.py
+++ outgoingslack/views.py
@@ -3,6 +3,8 @@ from django.utils import timezone
 from .models import OutGoingSlack
 from rest_framework import viewsets
 from .serializers import SlackSerializer
+from selenium import webdriver
+from bs4 import BeautifulSoup
 
 # Create your views here.
 
@@ -15,7 +17,7 @@ def post_list(request):
 def save_slack_msg(request):
     if request.method == "GET":
         print("잘못된 입력!")
-        return
+
 
 
 
@@ -23,3 +25,42 @@ def save_slack_msg(request):
 class SlackViewSet(viewsets.ModelViewSet):
     queryset = OutGoingSlack.objects.all()
     serializer_class = SlackSerializer
+
+
+def getCommitList():
+    driver = webdriver.Chrome(executable_path="../chromedriver")
+    driver.get('https://app.slack.com/client/TS08LHC73/CSAFNFB4M')
+
+    driver.find_element_by_id('domain').send_keys('githubfarm')
+    driver.find_element_by_xpath('//*[@id="submit_team_domain"]').click()
+
+    driver.find_element_by_name('email').send_keys('i3048i@yonsei.ac.kr')
+    driver.find_element_by_xpath('//*[@id="signin_btn"]').click()
+
+
+    req = driver.page_source
+    soup = BeautifulSoup(req, 'html.parser')
+
+    message_list = soup.select('div.c-message_attachment__body')
+
+    commit_list = []
+
+    for i in range(len(message_list)):
+        author_img = message_list[i].select('img.c-message_attachment__author_icon')
+        author = message_list[i].select('span.c-message_attachment__author_name')
+        msg = message_list[i].select('span.c-message_attachment__text')
+        if "1 new commit" in msg[0].text:
+            print(msg[0].text)
+            commit_list.append(message_list[i])
+
+    return commit_list
+
+
+def saveCommitList():
+    commit_list = getCommitList()
+
+    for one_commit in commit_list:
+        cd_author = one_commit.select_one('span.c-message_attachment__author_name').text
+        cd_msg = one_commit.select_one('span.c-message_attachment__text > span') #html 코드
+        cd_repository = one_commit.select_one('span.c-message_attachment__footer_text > span > a').text
+        cd_respositoryUrl = one_commit.select_one('span.c-message_attachment__footer_text > span > a').get('href')
